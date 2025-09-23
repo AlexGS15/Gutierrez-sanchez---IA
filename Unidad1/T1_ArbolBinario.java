efc0b5c949c96f233b54c2635ed0b25749eb5e8e
edgar alexis gutierrez sanchez <100734316+AlexGS15@users.noreply.github.com>
2025-09-23 08:19:28 -0700

    Update T1_ArbolBinario.java

--- Unidad1/T1_ArbolBinario.java
+++ Unidad1/T1_ArbolBinario.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class T1_Arbolbinario {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Arbol arbol = new Arbol();
@@ -102,3 +102,4 @@ class Arbol {
         }
     }
 }
+
